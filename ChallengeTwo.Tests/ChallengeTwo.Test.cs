706e0f28a8dad93c0d7da7b9133afc4a342cffe0
Parker Anderson <parkeranderson.dev@gmail.com>
2020-12-17 20:53:39 -0500

    Challenge Two Tests Passed

---
+++ ChallengeTwo.Tests/ChallengeTwo.Test.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ChallengeTwo.Repo;
+using System.Collections.Generic;
+
+namespace ChallengeTwo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Queue<Claim> _claims = new Queue<Claim>();
+        [TestMethod]
+        public void Test_SeeAllClaims()
+        {
+            var claimrepo = new ClaimRepo();
+            claimrepo.SeeAllClaims();
+            Assert.IsNotNull(_claims);
+        }
+        public void Test_WorkNextClaim()
+        {
+            var claimrepo = new ClaimRepo();
+            var initialclaims = _claims.Count;
+            claimrepo.WorkNextClaim();
+            Assert.AreNotEqual(initialclaims, _claims.Count);
+        }
+        public void Test_CreateClaim()
+        {
+            var claimrepo = new ClaimRepo();
+            var initialcount = _claims.Count;
+            var testclaim = new Claim();
+            claimrepo.CreateClaim(testclaim);
+            int newcount = _claims.Count;
+            Assert.AreNotEqual(initialcount, newcount);
+        }
+    }
+}
